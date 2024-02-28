c88225d7c96866dc4039ab59d6391ceb9d6c0d38
GabriellaPandiangan <gabriellapandiangan7@gmail.com>
2024-02-28 10:30:29 +0700

    upload

---
+++ coba.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <set>
+
+bool cekDuplikat (int arr[], int panjangArr) {
+    std::set <int> setArr;
+    for (int i = 0; i < panjangArr; i++) {
+        if (setArr.find(arr[i]) != setArr.end()) {
+            return true;
+        }
+        setArr.insert(arr[i]);
+    }
+    return false;
+}
+int main(void) {
+    int arrA[] = {2,1,3,1};
+    std::cout << cekDuplikat(arrA, 4);
+    return 0;
+}
